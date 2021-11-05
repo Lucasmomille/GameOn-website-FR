a39d8a7462778b730f645de6476a1aafd809f492
Lucas <lucas.moquet95@gmail.com>
2021-11-05 18:09:07 +0100

    project done

--- starterOnly/form.js
+++ starterOnly/form.js
@@ -1,72 +1,103 @@
-const firstname = document.getElementById('first');
-const lastname = document.getElementById('last');
-const birhday = document.getElementById('birthdate');
-const quantity = document.getElementById('quantity');
-const email = document.getElementById('email');
-const checkboxes = Array.from(document.querySelectorAll(".checkbox-input"));
-const form = document.getElementById('form');
-const submitted = document.getElementById('submitted');
+const firstname = document.getElementById('first')
+const lastname = document.getElementById('last')
+const birthday = document.getElementById('birthdate')
+const quantity = document.getElementById('quantity')
+const email = document.getElementById('email')
+const checkboxes = Array.from(document.querySelectorAll('.checkbox-input'))
+const condition = document.getElementById('checkbox1')
+const form = document.getElementById('form')
+const submitted = document.getElementById('submitted')
 
-const isChecked = checkboxes.reduce((acc, curr) => acc || curr.checked, false);
+const minTwoChars = (elt) => {
+  const onlyLetters = /^[a-zA-Z]+$/
+  if (elt.value.length > 1 && onlyLetters.test(elt.value)) {
+    elt.parentNode.dataset.errorVisible = false
+    return true
+  } else {
+    elt.parentNode.dataset.errorVisible = true
+  }
+}
+const onlyNumber = (elt) => {
+  const onlyNumbers = /^[0-9]*$/
+  if (elt.value.length > 0 && onlyNumbers.test(elt.value)) {
+    elt.parentNode.dataset.errorVisible = false
+    return true
+  } else {
+    elt.parentNode.dataset.errorVisible = true
+  }
+}
+const validateEmail = (email) => {
+  // eslint-disable-next-line no-useless-escape
+  const validEmailAdress = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
-const errorFirstname = document.getElementById('errorFirst');
-const errorLastname = document.getElementById('errorLast');
-const errorQuantity = document.getElementById('errorQuantity');
-const errorEmail = document.getElementById('errorEmail');
-const errorBirthday = document.getElementById('errorBirthday');
-
-//Regex for form's validation
-const minTwoCharacter = /^.{2,}$/;
-const validEmailAdress = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const onlyNumber = /^[0-9]*$/;
-/* 
-//Error messages
-let errorMessages = {
-    minCharacters: "Minimum deux caractères",
-    email: "L'adresse mail doit être valide",
-    number: "Seulement des chiffres"
-} */
-
-//handle error
-
-
-
-const minTwoChars = (elt, errorMessage) => {
-    elt.addEventListener("input", function() {
-        if (elt.value.length > 1){
-            errorMessage.classList.add("none");
-            elt.classList.remove("border-error");
-        } else {
-            console.log("error1", errorLastname)
-            errorMessage.classList.remove("none");
-            elt.classList.add("border-error");
-        }
-    })
+  if (validEmailAdress.test(email.value.toLowerCase())) {
+    email.parentNode.dataset.errorVisible = false
+    return true
+  } else {
+    email.parentNode.dataset.errorVisible = true
+  }
 }
 
-const validateEmail = () => {
-    if (validEmailAdress.test(email.value.toLowerCase())) {
-        errorEmail.classList.add("none")
-        validEmailAdress.classList.remove("border-error")
-    } else {
-        errorEmail.classList.remove("none")
-        validEmailAdress.classList.remove("border-error")
-    }
+const validateBirthday = (birthday) => {
+  const isDate = (date) => {
+    return (new Date(date) !== 'Invalid Date') && !isNaN(new Date(date))
+  }
+  if (birthday.value.length > 0 && isDate(birthday)) {
+    birthday.parentNode.dataset.errorVisible = false
+    return true
+  } else {
+    birthday.parentNode.dataset.errorVisible = true
+  }
 }
 
+const validateArrayOfCheckboxes = (checkboxes) => {
+  const checkboxeCheck = checkboxes.reduce((acc, curr) => acc || curr.checked, false)
+  if (checkboxeCheck) {
+    checkboxes[0].parentNode.dataset.errorVisible = false
+    return true
+  } else {
+    checkboxes[0].parentNode.dataset.errorVisible = true
+  }
+}
 
-form.addEventListener('submit', function(e){
-        e.preventDefault();
-        
-        minTwoChars(firstname, errorFirstname);
-        minTwoChars(lastname, errorLastname);
-        validateEmail();
-    
-        if (birhday.value.length > 0){
-            errorBirthday.classList.add("none")
-        } else {
-            errorBirthday.classList.remove("none")
-        }
-        console.log("check", isChecked)
-    }
+const validateOneCheckboxe = (checkboxe) => {
+  if (checkboxe.checked) {
+    checkboxe.parentNode.dataset.errorVisible = false
+    return true
+  } else {
+    checkboxe.parentNode.dataset.errorVisible = true
+  }
+}
+
+// Validate form
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault()
+  if (validateForm()) {
+    submitted.classList.remove('none')
+    form.classList.add('none')
+  } else {
+    submitted.classList.add('none')
+    form.classList.remove('none')
+  }
+}
 )
+
+// Check every input
+function validateForm () {
+  const arrayOfFunctions = [
+    minTwoChars(firstname),
+    minTwoChars(lastname),
+    onlyNumber(quantity),
+    validateEmail(email),
+    validateArrayOfCheckboxes(checkboxes),
+    validateOneCheckboxe(condition),
+    validateBirthday(birthday)
+  ]
+  const checkArrayTrue = arrayOfFunctions.every(v => v === true)
+  if (checkArrayTrue) {
+    return true
+  } else {
+    return false
+  }
+}
--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,28 +1,31 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+/* function editNav () {
+  const x = document.getElementById('myTopnav')
+  if (x.className === 'topnav') {
+    x.className += ' responsive'
   } else {
-    x.className = "topnav";
+    x.className = 'topnav'
   }
-}
+} */
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const modalCloseBtn = document.querySelectorAll(".close");
+const modalbg = document.querySelector('.bground')
+const modalBtn = document.querySelectorAll('.modal-btn')
+const btnClose = document.querySelectorAll('.btn-close')
+const modalCloseBtn = document.querySelectorAll('.close')
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach((btn) => btn.addEventListener('click', launchModal))
 
 // launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
+function launchModal () {
+  modalbg.style.display = 'block'
 }
 
-modalCloseBtn.forEach((btn) => btn.addEventListener("click", closeModal));
+// Close modal
+modalCloseBtn.forEach((btn) => btn.addEventListener('click', closeModal))
+btnClose.forEach((btn) => btn.addEventListener('click', closeModal))
 
-function closeModal () {
-  modalbg.style.display = "none";
+function closeModal (e) {
+  e.preventDefault()
+  modalbg.style.display = 'none'
 }
